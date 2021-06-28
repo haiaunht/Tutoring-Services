8d81edb7401cb8c33a1d7f4f8872608bb9739ded
Hai-Au Bui <haiaunht@yahoo.com>
2021-06-28 13:56:03 -0700

    add InstructorList component and card

---
+++ src/main/frontend/client/components/CourseCard/InstructorCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const InstructorCard = (props) => {
+  const {
+    id,
+    firstName,
+    lastName,
+    email,
+    phone,
+    role,
+    link,
+  } = props;
+
+
+  return (
+    <>
+      <Link to={link} className="card">
+        <div className="card__course-content">
+          <h3 className="card__course-title">{firstName} {lastName}</h3>
+          <div className="card__course-description">{email}</div>
+          <div className="card__course-instructor">{phone}</div>
+          <div className="card__course-row">More detail</div>
+        </div>
+        <div className="button card__course-button"><i className="fas fa-plus"></i></div>
+      </Link>
+    </>
+  );
+};
+
+export default InstructorCard;
--- src/main/frontend/client/components/InstructorsList.js
+++ src/main/frontend/client/components/InstructorsList.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import InstructorCard from "./CourseCard/InstructorCard";
 
 const InstructorsList = props => {
+  const [instructorsList, setInstructorsList] = useState([])
+
+  const fetchAllInstructors = async () => {
+    try {
+      const response = await fetch(`/api/v1/instructors`)
+      if (!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`;
+        const error = new Error(errorMessage);
+        throw error;
+      }
+
+      const responseBody = await response.json()
+      setInstructorsList(responseBody)
+      console.log(responseBody)
+    } catch (err) {
+      console.log(`Error in fetch all instructors: ${error.message}`);
+    }
+  }
+
+  useEffect(() => {
+    fetchAllInstructors()
+  }, [])
+
+  const listOfInstructors = instructorsList.map(each => {
     return (
-        <div>
-          <h3>Here is the list</h3>
-        </div>
+      <InstructorCard
+          key={each.id}
+          id={each.id}
+          firstName={each.firstName}
+          lastName={each.lastName}
+          email={each.email}
+          phone={each.phone}
+          role={each.role.name}
+          link={`/instructors/${each.id}`}
+      />
+    )
+  })
+
+    return (
+        <section className="course section container" id="course">
+          <span className="section-subtitle">Our Instructors</span>
+          <h2 className="section-title">Find the right instructor for you</h2>
+          <div className="course__container grid">{listOfInstructors}</div>
+        </section>
     )
 }
 
