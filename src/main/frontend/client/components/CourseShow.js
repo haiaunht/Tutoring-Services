6b73876fbacb3eccea13b1f180b130a7630003a0
Hai-Au Bui <haiaunht@yahoo.com>
2021-06-29 08:48:03 -0700

    show instructors under course

--- src/main/frontend/client/components/CourseShow.js
+++ src/main/frontend/client/components/CourseShow.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from "react";
 import {Link, Redirect} from "react-router-dom";
+import InstructorShow from "./InstructorShow";
+import InstructorsList from "./InstructorsList";
 
 const CourseShow = props => {
   const [courseDetail, setCourseDetail] = useState([])
@@ -31,19 +33,36 @@ const CourseShow = props => {
     return <Redirect to={"/404"} />
   }
 
+  const [showInstructor, setShowInstructor] = useState(false)
+  const showCard = event => {
+    event.preventDefault()
+    setShowInstructor(true)
+  }
+
+  let display = ""
+  if (showInstructor) {
+    display = <InstructorsList />
+  }
+
   return (
-    <div >
-      <div className="courseDiv">
-        <h1 className="middle">{courseDetail.name}</h1>
-        <h5>{courseDetail.description}</h5>
-        <h1 className="middle"><img src={courseDetail.imageUrl} /></h1>
-        <h3>Rating: {courseDetail.rating}</h3>
-        <h3>Tutoring price per hour: {courseDetail.price}</h3>
-      </div>
-      <div>
-        <button href="/instructors/all"><Link to={`/instructors/all`}>Find Instructor for this course!</Link></button>
-      </div>
-    </div>
+      <>
+        <div >
+          <div className="courseDiv">
+            <h1 className="middle">{courseDetail.name}</h1>
+            <h5>{courseDetail.description}</h5>
+            <h1 className="middle"><img src={courseDetail.imageUrl} /></h1>
+            <h3>Rating: {courseDetail.rating}</h3>
+            <h3>Tutoring price per hour: {courseDetail.price}</h3>
+          </div>
+          <div onClick={showCard}>
+            <button>Find Instructor for this course!</button>
+            {/*<button href="/instructors/all"><Link to={`/instructors/all`}>Find Instructor for this course!</Link></button>*/}
+          </div>
+        </div>
+          <div>
+            {display}
+          </div>
+      </>
   )
 }
 
