138c5193121823fd026e3bb17a09e7ef8cf617b0
yauacom <70412615+yauacom@users.noreply.github.com>
2021-07-21 23:51:23 -0700

    added monthy newsletter

--- src/main/frontend/client/utils/HelperFunctions.js
+++ src/main/frontend/client/utils/HelperFunctions.js
@@ -39,3 +39,9 @@ export const postData = async (url, formPayload) => {
   }
   return {};
 };
+
+export const isEmailValid = (email) => {
+  const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  console.log(regex.test(email));
+  return regex.test(email);
+};
